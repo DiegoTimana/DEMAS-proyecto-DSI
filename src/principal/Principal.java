9d02e4b274b3611990a1eb7ea9f5b97f04f7b894
luisfelipevalencia <70112728+luisfelipevalencia@users.noreply.github.com>
2021-09-20 19:38:48 -0500

    LuisValencia-realize cambios para arreglar el login

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,17 +1,6 @@
 package principal;
 
-
-import java.sql.*;
-
-
 import controlador.ControladorLogin;
-import controlador.ControladorSedes;
-import modelo.Con_postgresql;
-import modelo.Conexion;
-import modelo.ConsultasSede;
-import modelo.Sede;
-import vista.VentanaGestionSedes;
-import vista.VentanaGestionUsuarios;
 import vista.VentanaLogin;
 
 public class Principal {
@@ -19,42 +8,14 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		try {
-			Connection conex = Con_postgresql.getConnection();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 
 		
-		/*
 		VentanaLogin ventanaInicio = new VentanaLogin();
 		ControladorLogin ctrl = new ControladorLogin(ventanaInicio);
 		
 		ctrl.iniciar();
 		ventanaInicio.setVisible(true);
-		*/
 		
-		
-		/*
-		Sede sede = new Sede();
-        ConsultasSede consultaSede = new ConsultasSede();
-        VentanaGestionSedes ventanaSede = new VentanaGestionSedes();
-        
-        ControladorSedes ctrl = new ControladorSedes(sede, consultaSede, ventanaSede);
-        
-        ctrl.iniciar();
-        ventanaSede.setVisible(true);
-        */
-		
-		VentanaLogin ventanaLogin1 = new VentanaLogin();
-		ventanaLogin1.setVisible(true);
-		ventanaLogin1.setLocationRelativeTo(null);
-		
-		
-		// este comentario lo hizo luis f
-		// este comentario lo hizo diego
 		
 	}
 
